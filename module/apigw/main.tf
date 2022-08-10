741d42f9af86faacf152c46b8e10ebbb40af2801
Kavan-Dalwadi <kdalwadi28@gmail.com>
2022-08-10 15:52:32 +0530

    [Fixed] Bugs in IAM

--- module/apigw/main.tf
+++ module/apigw/main.tf
@@ -29,7 +29,7 @@ resource "aws_api_gateway_integration" "integration" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = var.function_name.name
+  function_name = var.function_name.function_name
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
